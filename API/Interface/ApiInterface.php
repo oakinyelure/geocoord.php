54e35ae22366b21794c586f1709b75fa0824242d
Olusegun Akinyelure <makinyelure@keteres.com>
2017-11-22 10:36:31 -0500

    Added Google API controllers

---
+++ API/Interface/ApiInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Michael Akinyelure
+ * Date: 11/22/2017
+ * Time: 10:33 AM
+ */
+
+namespace API;
+
+
+interface ApiInterface
+{
+
+    public function setGoogleURL();
+    public function checkURLStatus();
+
+}
